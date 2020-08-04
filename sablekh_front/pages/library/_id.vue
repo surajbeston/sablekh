95843497c1ba5c8e3a660e779acb143ef75ddf28
SauravNiraula <sauravniraula912@gmail.com>
2020-08-04 13:49:27 +0545

    library

---
+++ sablekh_front/pages/library/_id.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="library">
+        <Library :lib_id="this.$route.params.id" />
+        <Footer />   
+    </div> 
+</template>
+
+<script>
+
+export default {
+//    mounted() {
+//        if (!this.$route.params.id){
+//            window.location.replace("/")
+//        }
+//    } 
+}
+
+</script>
+
+<style scoped>
+
+</style>
